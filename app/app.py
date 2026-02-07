8cb6bf608a9e392105753acbc76af47275941612
rushikeshpanchal <rushikeshpanchal196@gmail.com>
2026-02-07 16:15:51 +0530

    Final Look up

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Yes we did it 🫠 CloudShip 🚢 is smoothly running on Kubernetes!"
+    return "Yes we did it 😍🫠 CloudShip 🚢 is smoothly running on Kubernetes!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
